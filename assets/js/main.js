00e1591fd78c1e8ba945397cb5164ac69f37d6ba
malabya <malabya35@gmail.com>
2017-07-17 09:33:23 +0530

    Styling changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,21 @@ function hasScrolled() {
 
   lastScrollTop = st;
 }
+
+
+// Load Disqus on demand.
+$(document).ready(function() {
+  $('.show-comments').on('click', function(){
+    var disqus_shortname = 'imalabya'; // Replace this value with *your* username.
+
+    // ajax request to load the disqus javascript
+    $.ajax({
+      type: "GET",
+      url: "http://" + disqus_shortname + ".disqus.com/embed.js",
+      dataType: "script",
+      cache: true
+    });
+    // hide the button once comments load
+    $(this).fadeOut();
+  });
+});
