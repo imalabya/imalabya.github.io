3c8cae0d4d974f22257e50e38c81ac23497ec05d
malabya <malabya35@gmail.com>
2017-08-02 21:14:03 +0530

    Design changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,11 @@ if ( window.location.pathname == '/' ) {
     density: 'medium'
   };
   var particleCanvas = new ParticleNetwork(canvasDiv, options);
+  $(".rotate").textrotator({
+    animation: "flipUp", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+    separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+    speed: 5000 // How many milliseconds until the next word show.
+  });
 }
 
 // Site header slide-in effect
